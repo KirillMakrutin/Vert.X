2a05d1ac55a64265f2268ea462ff929713b77af6
Kirill Makrutin <k.makrutin@gmail.com>
2018-09-30 11:13:09 +0300

    Init message

---
+++ src/main/java/com/kmakrutin/vertxchat/VertxChatApplication.java
@@ -0,0 +1,12 @@
+package com.kmakrutin.vertxchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VertxChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VertxChatApplication.class, args);
+    }
+}
